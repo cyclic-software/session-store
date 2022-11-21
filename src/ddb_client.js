f0cba7c57d84c26b0d78c7fb166f8b3b55ddb7b9
Mike <korostelevm@gmail.com>
2022-11-21 10:30:36 -0500

    v3 sdk

---
+++ src/ddb_client.js
@@ -0,0 +1,31 @@
+
+const { DynamoDBDocumentClient } = require("@aws-sdk/lib-dynamodb")
+const { DynamoDBClient } = require("@aws-sdk/client-dynamodb")
+
+const REGION = process.env.AWS_REGION || 'us-east-2'
+const ddbClient = new DynamoDBClient({ region: REGION });
+
+const marshallOptions = {
+    // Whether to automatically convert empty strings, blobs, and sets to `null`.
+    convertEmptyValues: false, // false, by default.
+    // Whether to remove undefined values while marshalling.
+    removeUndefinedValues: true, // false, by default.
+    // Whether to convert typeof object to map attribute.
+    convertClassInstanceToMap: true, // false, by default.
+};
+
+const unmarshallOptions = {
+    // Whether to return numbers as a string instead of converting them to native JavaScript numbers.
+    wrapNumbers: false, // false, by default.
+};
+
+const translateConfig = { marshallOptions, unmarshallOptions };
+
+const docClient = DynamoDBDocumentClient.from(ddbClient, translateConfig);
+
+
+
+
+module.exports = {
+    docClient
+}
